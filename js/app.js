9e44a9eedd84563b9c60e7c1e51b8841fda2d946
John <ema8720@gmail.com>
2019-01-02 21:41:44 -0800

    final

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 // const colors array
 const colors = ['yellow', 'green', 'blue','#f15025', 'rgba(125,125,125,0.5)'];
 // target the button
-const btn = getElementById('btn');
+const btn = document.getElementById('btn');
+// add event listener with an annonymous function
+btn.addEventListener('click', function () {
+    // create random 
+let random = Math.floor(Math.random() * colors.length);
+// select the body of the dom
+const body = document.body;
+body.style.backgroundColor = colors[random];
 
+})
